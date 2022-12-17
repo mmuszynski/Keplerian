049684da8c9f8f915500a30d1d3e764d0577724f
Mike Muszynski <mmuszynski@gmail.com>
2022-12-17 10:12:38 -0500

    getting ready to add maneuvvers

---
+++ Keplerian/Orbit+Maneuver.swift
@@ -0,0 +1,17 @@
+//
+//  File.swift
+//  
+//
+//  Created by Mike Muszynski on 12/15/22.
+//
+
+import Foundation
+
+extension Orbit {
+    struct Maneuver {
+        var progradeDV: Double = 0
+        var normalDV: Double = 0
+        var radialDV: Double = 0
+        var time: Double = 0
+    }
+}
